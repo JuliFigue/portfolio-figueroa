e9059a550ea752cf0cd6932236d37cfd9f7059a5
JuliFigue <julietafigueroa91@gmail.com>
2025-10-26 20:58:42 -0300

    galeria- prueba modal

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const audioElement = document.getElementById('musicaFondo');
     const playButton = document.getElementById('btnMusica');
     let isPlaying = false; 
-
-    
     function togglePlay() {
         if (isPlaying) {
             audioElement.pause();
@@ -22,14 +20,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         isPlaying = !isPlaying;
     }
-
-    
     playButton.addEventListener('click', togglePlay);
-
-    
     audioElement.addEventListener('ended', () => {
         isPlaying = false;
         playButton.innerHTML = '▶ Poné play!';
         playButton.classList.remove('is-playing');
     });
+});
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modal = document.getElementById('imageModal');
+    if (!modal) return; 
+    
+    var modalImg = document.getElementById('modalImage'); 
+
+    var galleryImages = document.querySelectorAll('.afiches-grid img, .fotos-grid img');
+
+    galleryImages.forEach(function(img) {
+        img.classList.add('galeria-img-preview'); 
+        
+        img.onclick = function() {
+            var fullSrc = this.getAttribute('src');
+            modal.classList.add('show');
+            modalImg.src = fullSrc;
+        }
+    });
+    
+    modal.onclick = function(event) {
+       
+        if (event.target === event.currentTarget || event.target.classList.contains('modal-content-wrapper')) { 
+            modal.classList.remove('show');
+        }
+    }
 });
